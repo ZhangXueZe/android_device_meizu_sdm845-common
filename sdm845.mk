5d8c8a5a9e4429d48c49e663f73e01ecc7948956
qianyf <f123456>
2019-01-16 09:43:37 +0800

    Revert "sdm845-common: mokee-ify"
    This reverts commit f72d80ed7c83561e77d797ef34f53b90262b4d4a.

--- sdm845.mk
+++ sdm845.mk
@@ -14,7 +14,7 @@ $(call inherit-product-if-exists, vendor/meizu/sdm845-common/sdm845-common-vendo
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-mokee
+    $(LOCAL_PATH)/overlay-lineage
 
 # Properties
 -include $(LOCAL_PATH)/common-props.mk
